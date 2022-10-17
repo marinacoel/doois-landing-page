25b990c40d9976fddc1c60c1d31abd1d27459f8d
Marina Coelho <coelhomarinaalves@gmail.com>
2022-10-17 11:28:54 -0300

    Adicionei componentes em layout

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -3,34 +3,25 @@ import Box from '@mui/material/Box'
 import Fab from '@mui/material/Fab'
 import Typography from '@mui/material/Typography'
 import { Header } from '../header/Header'
+import { Section } from '../section/Section'
 
 export const Layout = () => {
-    return (
-        <>
-        <Box sx={{ flexGrow: 1 }}>
-            <Header />
+  return (
+    <>
+      <Header />
 
-        </Box>
-        <Box sx={{ p: 3 }}>
-            <Typography variant="h1">h1. Heading</Typography>
-            <Typography variant="h2">h2. Heading</Typography>
-            <Typography variant="h3">h3. Heading</Typography>
-            <Typography variant="h4">h4. Heading</Typography>
-            <Typography variant="h5">h5. Heading</Typography>
-            <Typography variant="h6">h6. Heading</Typography>
-            <Typography variant="body1">body1. text</Typography>
-            <Typography variant="body2">body2. text</Typography>
-        </Box>
-        <Fab
-            color="secondary"
-            sx={{
-            position: 'fixed',
-            right: ({ spacing }) => spacing(3),
-            bottom: ({ spacing }) => spacing(3),
-            }}
-        >
-            <AddIcon />
-        </Fab>
-        </>
-    )
+      <Section />
+
+      <Fab
+        color="secondary"
+        sx={{
+          position: 'fixed',
+          right: ({ spacing }) => spacing(3),
+          bottom: ({ spacing }) => spacing(3),
+        }}
+      >
+        <AddIcon />
+      </Fab>
+    </>
+  )
 }
