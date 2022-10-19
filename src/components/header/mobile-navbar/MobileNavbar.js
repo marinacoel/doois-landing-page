87153100518ce727d392d6bcc5f5c4d729106682
Marina Coelho <coelhomarinaalves@gmail.com>
2022-10-19 15:31:05 -0300

    Implementei scroll to section no drawer

--- src/components/header/mobile-navbar/MobileNavbar.js
+++ src/components/header/mobile-navbar/MobileNavbar.js
@@ -2,6 +2,7 @@ import { Drawer, IconButton, Typography } from '@mui/material'
 import CloseIcon from '@mui/icons-material/Close'
 import MenuIcon from '@mui/icons-material/Menu'
 import { Button } from '@mui/material'
+
 import clsx from 'clsx'
 
 import { useState } from 'react'
@@ -11,6 +12,7 @@ import { mobileNavbarStyles } from './mobileNavbar.styles'
 export const MobileNavbar = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false)
   const classes = mobileNavbarStyles()
+
   const scrollToSection = elementId => {
     const elementToScrollTo = document.getElementById(elementId)
     elementToScrollTo.scrollIntoView({
@@ -18,11 +20,18 @@ export const MobileNavbar = () => {
     })
   }
 
+  const handleClick = sectionName => {
+    scrollToSection(sectionName)
+    setIsDrawerOpen(false)
+  }
+
   return (
-    <div className={classes.mobile_navbar}>
-      <IconButton className={classes.mn_menu_button} onClick={() => setIsDrawerOpen(true)}>
-        <MenuIcon />
-      </IconButton>
+    <>
+      <div className={classes.mobile_navbar}>
+        <IconButton className={classes.mn_menu_button} onClick={() => setIsDrawerOpen(true)}>
+          <MenuIcon />
+        </IconButton>
+      </div>
       <Drawer anchor="left" open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
         <div className={classes.mn_menu}>
           <IconButton
@@ -36,7 +45,7 @@ export const MobileNavbar = () => {
               variant="body1"
               color="primary.white"
               className={classes.mn_txt}
-              onClick={() => scrollToSection('home')}
+              onClick={() => handleClick('home')}
             >
               Home
             </Typography>
@@ -44,7 +53,7 @@ export const MobileNavbar = () => {
               variant="body1"
               color="primary.white"
               className={classes.mn_txt}
-              onClick={() => scrollToSection('how')}
+              onClick={() => handleClick('how')}
             >
               How to use
             </Typography>
@@ -52,7 +61,7 @@ export const MobileNavbar = () => {
               variant="body1"
               color="primary.white"
               className={classes.mn_txt}
-              onClick={() => scrollToSection('about')}
+              onClick={() => handleClick('about')}
             >
               About us
             </Typography>
@@ -60,7 +69,7 @@ export const MobileNavbar = () => {
               variant="body1"
               color="primary.white"
               className={classes.mn_txt}
-              onClick={() => scrollToSection('services')}
+              onClick={() => handleClick('services')}
             >
               Services
             </Typography>
@@ -68,7 +77,7 @@ export const MobileNavbar = () => {
               variant="body1"
               color="primary.white"
               className={classes.mn_txt}
-              onClick={() => scrollToSection('contact')}
+              onClick={() => handleClick('contact')}
             >
               Contact
             </Typography>
@@ -78,6 +87,6 @@ export const MobileNavbar = () => {
           </div>
         </div>
       </Drawer>
-    </div>
+    </>
   )
 }
