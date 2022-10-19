352c90181e890ee9007ec3c9d3f0728a04de9bb9
Marina Coelho <coelhomarinaalves@gmail.com>
2022-10-19 15:31:54 -0300

    Adicionei função de ElevationScroll em header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,35 @@
-// import { DesktopNabvar } from './desktop-navbar/DesktopNavbar'
 import { MobileNavbar } from './mobile-navbar/MobileNavbar'
+import { cloneElement } from 'react'
+import { Button, useScrollTrigger } from '@mui/material'
 import { DesktopNabvar } from './desktop-navbar/DesktopNavbar'
 
-import { headerStyles } from './header.styles'
-import { useTheme, useMediaQuery } from '@mui/material';
+import { useTheme, useMediaQuery, AppBar } from '@mui/material'
 
-export const Header = () => {
+import { headerStyles } from './header.styles'
+
+function ElevationScroll(props) {
+  const { children, window } = props
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  })
+
+  return cloneElement(children, {
+    elevation: trigger ? 3 : 0,
+  })
+}
+
+export const Header = props => {
   const classes = headerStyles()
-	const theme = useTheme()
-	const mobileViewport = useMediaQuery(theme.breakpoints.down('md'));
+  const theme = useTheme()
+  const mobileViewport = useMediaQuery(theme.breakpoints.down('md'))	
 
   return (
-		<div className={classes.header}>
-			{mobileViewport ? <MobileNavbar /> : <DesktopNabvar />}
-		</div>
+    <ElevationScroll {...props}>
+      <AppBar elevation={0} position="fixed" className={classes.header}>
+        {mobileViewport ? <MobileNavbar /> : <DesktopNabvar />}
+      </AppBar>
+    </ElevationScroll>
   )
 }
