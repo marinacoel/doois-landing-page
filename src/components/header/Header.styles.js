50d385cd295da9fbfccff05183fe5384c6eeccba
Marina Coelho <coelhomarinaalves@gmail.com>
2022-10-20 10:18:33 -0300

    Concertei header

--- src/components/header/Header.styles.js
+++ src/components/header/Header.styles.js
@@ -1,18 +1,18 @@
 import { makeStyles } from '@mui/styles'
 
 export const headerStyles = makeStyles((theme) => ({
-    header: {
-      boxSizing: 'border-box',
+    // header: {
+    //   boxSizing: 'border-box',
   
-      width: '100%',
-      height: '4.5rem',
+    //   width: '100%',
+    //   height: '4.5rem',
 
-      backgroundColor: theme.palette.primary.main,
+    //   backgroundColor: theme.palette.primary.main,
   
-      display: 'flex',
-      flexDirection:'row',
-      alignItems: 'center',
-      justifyContent: 'space-around',
-      padding: '24px',
-    },
+    //   display: 'flex',
+    //   flexDirection:'row',
+    //   alignItems: 'center',
+    //   justifyContent: 'space-around',
+    //   padding: '24px',
+    // },
   }))
